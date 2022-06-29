b3da25573c457cf3fed1507150458f8fdd9b0395
NikiforowN <kiwiwiwitsk@gmail.com>
2022-06-30 03:14:18 +0700

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,45 @@
+// Подключение основного файла стилей
+import "../scss/style.scss";
+
+// constants
+const header = document.querySelector('.header')
+const headerContainer = document.querySelector('.header__container')
+const burger = document.querySelector('#burger');
+const hedaerMenu = document.querySelector('.menu__body');
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+// header scroll
+window.addEventListener('scroll', () => {
+   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+      headerContainer.style.height = '3.75rem';
+      header.style.background = '#F6F6F6';
+   } else {
+      headerContainer.style.height = '';
+      header.style.background = '';
+   }
+});
+
+// burger menu
+burger.addEventListener('click', () => {
+   burger.classList.toggle('active');
+   hedaerMenu.classList.toggle('active');
+});
+
+// smooth scroll
+for (let anchor of anchors) {
+   anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const blockID = anchor.getAttribute('href').substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+         behavior: 'smooth',
+         block: 'start',
+      })
+
+      if (burger.classList.contains('active')) {
+         burger.classList.toggle('active');
+         hedaerMenu.classList.toggle('active');
+      }
+   })
+}
